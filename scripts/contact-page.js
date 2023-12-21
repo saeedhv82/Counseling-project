414735a54a1986838884e8e9c80641a792fe41a3
Saeed <saeedvakili1382@gmail.com>
2023-12-21 17:48:32 +0330

    new commit ...

---
+++ scripts/contact-page.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// hamburger menu functionality
+
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+    document.getElementById("menu-bar").classList.toggle("burger-bg");
+}
